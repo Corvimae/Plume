93e373ff5e16829d99f75361690168a2db576879
May Roussel <may@maybreak.com>
2016-03-03 14:54:21 -0600

    Initial commit.

--- CoreEngine/Program.cs
+++ CoreEngine/Program.cs
@@ -14,7 +14,7 @@ namespace CoreEngine
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new Core())
                 game.Run();
         }
     }
---
+++ CoreEngine/World/Map.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CoreEngine.World {
+	class Map {
+		protected int Width;
+		protected int Height;
+		private MapTile[,] grid;
+
+		public Map(int width, int height) {
+			this.Width = width;
+			this.Height = height;
+			grid = new MapTile[Height, Width];
+			for (int x = 0; x < grid.GetLength(0); x++) {
+				for (int y = 0; y < grid.GetLength(1); y++) {
+					grid[x, y] = new MapTile();
+				}
+			}
+		}
+
+		public void draw(SpriteBatch spriteBatch) {
+			for (int x = 0; x < grid.GetLength(0); x++) {
+				for (int y = 0; y < grid.GetLength(1); y++) {
+					MapTile tile = grid[x, y];
+					tile.draw(spriteBatch);
+				}
+			}
+		}
+	}
+}
