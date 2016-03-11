adbf7103cfc231e6796f80e0cdfd91f089589f95
May Roussel <may@maybreak.com>
2016-03-10 23:19:54 -0600

    Added some untracked files

---
+++ CoreEngine/Content/Modules/Core/Main.rb
@@ -0,0 +1,18 @@
+﻿include CoreEngine::Entities
+
+class Main
+	def before_load
+	end
+
+	def after_load
+		p "Core Module by AcceptableIce: Initialized"
+	end
+
+	def update
+
+	end
+	
+	def draw
+		
+	end
+end
